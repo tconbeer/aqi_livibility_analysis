147b6635ee47da351224da0de7ff8f1c13d24b61
tconbeer <tconbeer@makespace.com>
2021-09-20 13:58:48 -0600

    add transform to parquet

--- src/aqi_livibility_analysis/ops.py
+++ src/aqi_livibility_analysis/ops.py
@@ -1,7 +1,9 @@
 import gzip
 import warnings
-from typing import Any, List
+from typing import Any, BinaryIO, List, TextIO, Union
 
+import numpy as np
+import pandas as pd
 from dagster import ExperimentalWarning, FileHandle, op
 
 # ignore warnings related to using new API with op
@@ -27,3 +29,60 @@ def download_hourly_data(context: Any) -> List[FileHandle]:
         paths.append(path)
 
     return paths
+
+
+def _read_hourly_file_to_dataframe(
+    filepath_or_buffer: Union[str, BinaryIO, TextIO]
+) -> pd.DataFrame:
+    df = pd.read_csv(
+        filepath_or_buffer,
+        sep="|",
+        names=[
+            "observed_date",
+            "observed_time",
+            "site_id",
+            "site_name",
+            "gmt_offset",
+            "parameter_name",
+            "reporting_units",
+            "value",
+            "data_source_agency",
+        ],
+        index_col="site_id",
+        dtype={
+            "observed_date": str,
+            "observed_time": str,
+            "site_id": str,
+            "site_name": str,
+            "gmt_offset": np.int32,
+            "parameter_name": str,
+            "reporting_units": str,
+            "value": np.float64,
+            "data_source_agency": str,
+        },
+        parse_dates={"observed_at": [0, 1]},
+        infer_datetime_format=True,
+        cache_dates=True,
+        compression="gzip",
+    )
+
+    return df
+
+
+@op(required_resource_keys={"fs"})
+def transform_hourly_data(
+    context: Any, raw_files: List[FileHandle]
+) -> List[FileHandle]:
+    transformed_files: List[FileHandle] = []
+    fs = context.resources.fs
+
+    for p in [h.path for h in raw_files]:
+        raw_df = _read_hourly_file_to_dataframe(p)
+
+        transformed_bytes = raw_df.to_parquet(compression=None, index=False)
+
+        path = fs.write_data(data=transformed_bytes, ext="parquet")
+
+        transformed_files.append(path)
+
+    return transformed_files
--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 from typing import List
 
+import pandas as pd
 import pytest
-from dagster import FileHandle, build_solid_context, local_file_manager
+from dagster import FileHandle, LocalFileHandle, build_solid_context, local_file_manager
 from dagster_gcp import GCSFileHandle, gcs_file_manager
 
-from aqi_livibility_analysis.ops import download_hourly_data
+from aqi_livibility_analysis.ops import (
+    _read_hourly_file_to_dataframe,
+    download_hourly_data,
+    transform_hourly_data,
+)
 from aqi_livibility_analysis.resources import airnow_resource
 
 
@@ -52,3 +57,70 @@ def test_gcs_download_hourly_data() -> None:
 
     for p in [h.gcs_path for h in handles]:
         assert p.endswith(".gzip")
+
+
+class TestTransform:
+    @pytest.fixture
+    def raw_file_path(self) -> str:
+        test_dir = Path(__file__).parent
+        p = test_dir / "data" / "HourlyData_2019011713.dat.gzip"
+        return str(p)
+
+    def test_read_hourly_file_to_dataframe(self, raw_file_path: str) -> None:
+        df = _read_hourly_file_to_dataframe(raw_file_path)
+
+        assert df is not None
+        assert df.shape == (15136, 7)
+        assert "observed_at" in df.columns
+        assert "parameter_name" in df.columns
+        assert "value" in df.columns
+
+    def test_local_transform_hourly_data(self, raw_file_path: str) -> None:
+        h = LocalFileHandle(path=raw_file_path)
+        context = build_solid_context(resources={"fs": local_file_manager})
+        transformed_handles = transform_hourly_data(context, raw_files=[h])
+
+        assert transformed_handles is not None
+        assert len(transformed_handles) == 1
+
+        transformed_file_path = transformed_handles[0].path
+        df = pd.read_parquet(transformed_file_path)
+
+        assert df is not None
+        assert df.shape == (15136, 7)
+        assert "observed_at" in df.columns
+        assert "parameter_name" in df.columns
+        assert "value" in df.columns
+
+    @pytest.mark.slow
+    @pytest.mark.gcp
+    @pytest.mark.filterwarnings("ignore:The loop argument:DeprecationWarning")
+    @pytest.mark.filterwarnings(
+        "ignore:coroutine 'noop' was never awaited:RuntimeWarning"
+    )
+    def test_gcs_transform_hourly_data(self, raw_file_path: str) -> None:
+        h = LocalFileHandle(path=raw_file_path)
+        context = build_solid_context(
+            resources={
+                "fs": gcs_file_manager.configured(
+                    {
+                        "project": "aqi-livibility-analysis",
+                        "gcs_bucket": "aqi-raw-data",
+                        "gcs_prefix": "test/test_gcs_transform_hourly_data",
+                    }
+                )
+            }
+        )
+        transformed_handles = transform_hourly_data(context, raw_files=[h])
+
+        assert transformed_handles is not None
+        assert len(transformed_handles) == 1
+
+        transformed_file_path = transformed_handles[0].gcs_path
+        df = pd.read_parquet(transformed_file_path)
+
+        assert df is not None
+        assert df.shape == (15136, 7)
+        assert "observed_at" in df.columns
+        assert "parameter_name" in df.columns
+        assert "value" in df.columns
