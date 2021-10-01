2d9106cfaa84383d58014e6aa7542c753eabf3b4
tconbeer <tconbeer@makespace.com>
2021-10-01 13:08:17 -0600

    add msa mapping seed and staging model

---
+++ src/aqi_dbt/models/staging/stg_sites_msa_mapping.sql
@@ -0,0 +1,13 @@
+with
+    seed as (select * from {{ ref('seed_sites_msa_mapping') }}),
+    final as (
+
+        select
+            site_id,
+            msa_code,
+            msa_name,
+        
+        from seed
+
+    )
+select * from final
